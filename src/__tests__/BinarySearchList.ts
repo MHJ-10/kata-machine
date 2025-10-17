29abaca95454ceff0681b38d949bf1f1e4024532
MHJ-10 <mhjafari.dev@gmail.com>
2025-10-17 13:01:06 +0330

    implement binary search

--- src/__tests__/BinarySearchList.ts
+++ src/__tests__/BinarySearchList.ts
@@ -1,8 +1,8 @@
-import binary_fn from "@code/BinarySearchList"
+import binary_fn from "@code/BinarySearchList";
 
-test("binary search array", function() {
+const foo = [1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420];
 
-    const foo = [1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420];
+test("binary search array", function () {
     expect(binary_fn(foo, 69)).toEqual(true);
     expect(binary_fn(foo, 1336)).toEqual(false);
     expect(binary_fn(foo, 69420)).toEqual(true);
@@ -11,3 +11,11 @@ test("binary search array", function() {
     expect(binary_fn(foo, 0)).toEqual(false);
 });
 
+// test("recurive binary search", () => {
+//     expect(recursiveBinarySearch(foo, 69, 0, foo.length)).toEqual(true);
+//     // expect(recursiveBinarySearch(foo, 1336, 0, foo.length)).toEqual(false);
+//     // expect(recursiveBinarySearch(foo, 69420, 0, foo.length)).toEqual(true);
+//     // expect(recursiveBinarySearch(foo, 69421, 0, foo.length)).toEqual(false);
+//     // expect(recursiveBinarySearch(foo, 1, 0, foo.length)).toEqual(true);
+//     // expect(recursiveBinarySearch(foo, 0, 0, foo.length)).toEqual(false);
+// });
--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,44 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
+    do {
+        const midPoint = Math.floor(low + (high - low) / 2);
+        const value = haystack[midPoint];
+
+        if (needle === value) return true;
+
+        if (needle > value) {
+            low = midPoint + 1;
+        }
+
+        if (needle < value) {
+            high = midPoint;
+        }
+    } while (low < high);
+
+    return false;
 }
+
+// export const recursiveBinarySearch = (
+//     arr: number[],
+//     target: number,
+//     low: number,
+//     high: number,
+// ): boolean => {
+//     const mid = Math.floor(low + (high - low) / 2);
+//     const val = arr[mid];
+
+//     if (low < high) {
+//         if (val === target) return true;
+
+//         if (val > target) {
+//             recursiveBinarySearch(arr, target, low, mid);
+//         }
+
+//         if (val < target) {
+//             recursiveBinarySearch(arr, target, mid + 1, high);
+//         }
+//     }
+//     return false;
+// };
