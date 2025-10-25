6b473198b6dd145e40eceb496504cc0d124a52e4
MHJ-10 <mhjafari.dev@gmail.com>
2025-10-25 20:35:17 +0330

    implement bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,13 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
+
+// above code be faster than this => (let i = 0; i < arr.length) in large data
