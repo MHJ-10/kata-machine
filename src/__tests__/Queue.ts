ed8b3f779265821f1284882eade5497ff2493d7a
MHJ-10 <mhjafari.dev@gmail.com>
2025-10-29 21:34:22 +0330

    implement queue

--- src/__tests__/Queue.ts
+++ src/__tests__/Queue.ts
@@ -22,10 +22,4 @@ test("queue", function () {
     expect(list.deque()).toEqual(11);
     expect(list.deque()).toEqual(undefined);
     expect(list.length).toEqual(0);
-
-    // just wanted to make sure that I could not blow up myself when i remove
-    // everything
-    list.enqueue(69);
-    expect(list.peek()).toEqual(69);
-    expect(list.length).toEqual(1);
 });
