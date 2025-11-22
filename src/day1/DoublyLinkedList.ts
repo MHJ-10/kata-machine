23ff2d398153cb56f94ee296082490835f75aae6
MHJ-10 <mhjafari.dev@gmail.com>
2025-11-22 18:43:38 +0330

    implement linkedlist insert methods

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,27 +1,82 @@
+interface Node<T> {
+    value: T;
+    prev?: Node<T>;
+    next?: Node<T>;
+}
+
 export default class DoublyLinkedList<T> {
     public length: number;
-
-    
+    public head?: Node<T>;
 
     constructor() {
+        this.length = 0;
     }
 
     prepend(item: T): void {
+        const node: Node<T> = { value: item };
 
-}
+        this.length++;
+        if (!this.head) {
+            this.head = node;
+        }
+
+        node.next = this.head;
+        this.head.prev = node;
+        this.head = node;
+    }
     insertAt(item: T, idx: number): void {
+        if (idx === 0 || !this.length) {
+            this.prepend(item);
+        } else if (idx === this.length) {
+            this.append(item);
+        } else if (idx > this.length) {
+            throw new Error("idx is greater than list length");
+        } else {
+            this.length++;
+            const node: Node<T> = { value: item };
+            let current = this.head;
+            for (let i = 0; current && i < idx; i++) {
+                current = current?.next;
+            }
 
-}
+            if (current?.prev) {
+                const currentPrev = current.prev;
+                currentPrev.next = node;
+                node.prev = currentPrev;
+                node.next = current;
+                current.prev = node;
+            }
+        }
+    }
     append(item: T): void {
+        const node: Node<T> = { value: item };
 
-}
-    remove(item: T): T | undefined {
+        if (!this.head) {
+            this.prepend(item);
+        }
 
-}
-    get(idx: number): T | undefined {
+        this.length++;
 
-}
-    removeAt(idx: number): T | undefined {
+        let current = this.head;
 
+        for (let i = 0; i < this.length; i++) {
+            current = current?.next;
+        }
+
+        if (current?.next) {
+            current.next = node;
+            node.prev = current;
+        }
+    }
+    // remove(item: T): T | undefined {}
+    // get(idx: number): T | undefined {
+
+    // }
+    // removeAt(idx: number): T | undefined {}
 }
+
+const list = new DoublyLinkedList<number>();
+
+for (let i = 10; i >= 1; i--) {
+    list.prepend(i);
 }
