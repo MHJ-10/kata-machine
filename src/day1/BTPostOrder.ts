63e8ab7b3fd23cb5d913c804113de03a2b99c029
MHJ-10 <mhjafari.dev@gmail.com>
2025-11-30 22:30:07 +0330

    implement binary tree post order

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,11 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+const walk = (curr: BinaryNode<number>, path: number[]) => {
+    if (curr.left) walk(curr.left, path);
+    if (curr.right) walk(curr.right, path);
+    path.push(curr.value);
 
+    return path;
+};
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
