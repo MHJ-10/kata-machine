862c18a83c992902271abb788f81c4e2a79c5310
MHJ-10 <mhjafari.dev@gmail.com>
2025-11-30 22:13:23 +0330

    implement binary tree pre order

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,52 +1,11 @@
-type BinaryNode<T> = {
-    value: T;
-    left: BinaryNode<T> | null;
-    right: BinaryNode<T> | null;
+const walk = (curr: BinaryNode<number>, path: number[]) => {
+    path.push(curr.value);
+    if (curr.left) walk(curr.left, path);
+    if (curr.right) walk(curr.right, path);
+
+    return path;
 };
 
-const traversal = (tree: BinaryNode<number>,array:number[]) => {
-
-    
-}
-
 export default function pre_order_search(head: BinaryNode<number>): number[] {
-    return [];
+    return walk(head, []);
 }
-
-pre_order_search({
-    value: 1,
-    left: {
-        value: 7,
-        right: {
-            value: 6,
-            left: {
-                value: 5,
-                right: null,
-                left: null,
-            },
-            right: {
-                value: 11,
-                left: null,
-                right: null,
-            },
-        },
-        left: {
-            value: 2,
-            left: null,
-            right: null,
-        },
-    },
-    right: {
-        value: 9,
-        left: null,
-        right: {
-            value: 12,
-            left: {
-                value: 4,
-                left: null,
-                right: null,
-            },
-            right: null,
-        },
-    },
-});
