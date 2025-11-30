63b8711b3325d1aa093bf17bc5480c1dfcbdc619
MHJ-10 <mhjafari.dev@gmail.com>
2025-11-30 22:24:39 +0330

    implement binary tree in order

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,11 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+const walk = (curr: BinaryNode<number>, path: number[]): number[] => {
+    if (curr.left) walk(curr.left, path);
+    path.push(curr.value);
+    if (curr.right) walk(curr.right, path);
 
+    return path;
+};
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
